3af07f3d59cf0dcb62093b759e1c5db03274e272
Biplav Subedi <bplv@Biplavs-MacBook-Pro.local>
2018-05-07 16:12:31 +0545

    init

---
+++ old/includes/class-woo-ws-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://wpdrizzle.com/
+ * @since      1.0.0
+ *
+ * @package    Woo_Ws
+ * @subpackage Woo_Ws/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Woo_Ws
+ * @subpackage Woo_Ws/includes
+ * @author     Biplav Subedi <bplv.me@gmail.com>
+ */
+class Woo_Ws_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ templates/add-to-cart.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Simple product add to cart
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/add-to-cart/simple.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     3.0.0
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+global $product;
+if ( ! $product->is_purchasable() ) {
+	return;
+}
+
+echo wc_get_stock_html( $product );
+$cart = WC()->instance()->cart;
+$count = "0";
+
+if ( $product->is_in_stock() ) : ?>
+
+
+	<form class="cart" action="" method="post" enctype='multipart/form-data' id="<?php echo get_the_ID(); ?>">
+		<?php
+ 
+            $min  = '0';
+            $step = 'any';
+		   
+            echo woocommerce_quantity_input(array(
+                                'input_name'    => "quickbuy-".get_the_ID(),
+                                'max_value'     => $product->get_max_purchase_quantity(),
+                                'min_value'     => '0',
+                                'product_name'  => $product->get_name(),
+                                'step'          => $step
+                            ), $product, false );
+		?>
+
+		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+
+	</form>
+
+
+<?php endif; ?>
